97068584821d480c9626c82e32eb6eef68f53f0c
conorb5 <connorb5@vt.edu>
2018-01-18 16:48:39 -0500

    Fixed the PHP code to match the R example. Should now be capable of calculating storage and discharge simealtaneously via a bisection solution of the modified puls method

--- php/plugins/omRuntime_HydroRiser.class.php
+++ php/plugins/omRuntime_HydroRiser.class.php
@@ -99,9 +99,9 @@ class omRuntime_HydroRiser extends omRuntime_SubComponent {
     //error_log("RISER($this->state[runid] : Riser Head: $riser_head, riser_pipe_flow_head: $this->riser_pipe_flow_head, Riser Opening S = $this->riser_opening_storage");
     // Now, if max possible riser_head > 0 then we have at least some flow out of riser
     //error_log("S0 ($S0) + Qin ($Qin) = $S1"); 
-    if ($riser_head > 0) {
+    if ($riser_head > 0 and $riser_emerg_elev < 0) {
       // determine which orifice equation to use depending on riser_head
-      if ($riser_head > $this->riser_pipe_flow_head and $riser_emerg_head < 0) {
+      if ($riser_head > $this->riser_pipe_flow_head) {
         //error_log("Head = $riser_head - Pipe Flow");
         // pipe flow
         $riser_flow = 0.6 * $this->riser_length 
@@ -109,49 +109,94 @@ class omRuntime_HydroRiser extends omRuntime_SubComponent {
           * sqrt(2.0 * 32.2 * ($riser_head - (0.5 * $this->riser_length)))
         ;
         $riser_mode = 'pipe';
-      } elseif ($riser_emerg_head >= 0){
-			$riser_flow = (0.6 * $this->riser_length 
-          * $this->riser_diameter 
-          * sqrt(2.0 * 32.2 * ($riser_head - (0.5 * $this->riser_length))))
-		  + 3.1 * $this->riser_emerg_diameter * pow($riser_emerg_head, 1.5)
-			;
-			$riser_mode = 'Emergency';
-	  }else {
+      } else {
         //error_log("Head = $riser_head - Weir Flow");
         // weir flow 
         $riser_flow = 3.1 * $this->riser_diameter * pow($riser_head, 1.5);
         //error_log("$riser_flow = 3.1 * $this->riser_diameter * pow($riser_head, 1.5)");
         $riser_mode = 'weir';
-      }
-	  //error_log("Qin = $Qin : (Calc riser_flow = $riser_flow)"); 
-      $S1 = $S0 
-        + (($Qin - $flowby - $riser_flow) * $dt / 43560.0) 
-        + (1.547 * $discharge * $dt / 43560.0)  
-        + (1.547 * $refill * $dt / 43560.0) 
-        - (1.547 * $demand * $dt /  43560.0) 
-        - ($evap_acfts * $dt) 
-        + ($precip_acfts * $dt)
-      ;
-      //error_log("Qin - flowby - riser_flow = ($Qin - $flowby - $riser_flow)"); 
-      if ($S1 < $this->riser_opening_storage) {
-        // calculate intermediate value to see what riser flow WOULD be to make S1 = riser_opening_storage
-        $S00 = $S0 
-          + (($Qin - $flowby) * $dt / 43560.0) 
-          + (1.547 * $discharge * $dt / 43560.0)  
-          + (1.547 * $refill * $dt / 43560.0) 
-          - (1.547 * $demand * $dt /  43560.0) 
-          - ($evap_acfts * $dt) 
-          + ($precip_acfts * $dt)
-        ;
-        $riser_flow = ($S00 - $this->riser_opening_storage) * 43560.0 / $dt;
-        //error_log("(Adjusted riser_flow = $riser_flow)"); 
-        if ($riser_flow < 0 ) {
-          $riser_flow = 0;
-        }
 	  }
+    } elseif ($riser_emerg_head >= 0){
+		$riser_flow = (0.6 * $this->riser_length 
+        * $this->riser_diameter 
+        * sqrt(2.0 * 32.2 * ($riser_head - (0.5 * $this->riser_length))))
+		+ 3.1 * $this->riser_emerg_diameter * pow($riser_emerg_head, 1.5)
+		;
+		$riser_mode = 'Emergency';
 	} else {
-		$riser_flow = 0.0;
+	  $riser_flow = 0;
 	}
+	$x = 0; //Need a loop counter
+	//Establish guess iterators for:
+    $Si = 0;//A lower bound storage estimate
+    $Sn = $S1;//A storage iterator for within the loop
+    $riserP = 0;//A reference to previous riser flow
+
+	//Begin a loop that continue until storage is solved iterativley with outflow at a reasonable tolerance
+    while (abs(($Sn-$S0+$riser_flow*$dt/43560)-($Qin*$dt/43560)) > 0.0001){
+      $x = $x+1;//Count loops
+      //Need to now check if storage and flow are reasonable compared to Qin such that dS+outflow*dt=inflow*dt (MPM equation).
+      //Then a solution can be found with the bisection method with solution Sn lying in intervale (Si,S1)
+      if (abs(($Sn-$S0+$riser_flow*$dt/43560)-($Qin*$dt/43560)) > 0.0001){
+        $Sn = ($S1+$Si)/2;//Find the bisection of the upper and lower possible storages, S1 and Si respectivley
+		// calculate riser_head at this storage and stores as riser_flow
+		$this->storage_stage_area->lutype2 = 0; // a fix
+		$stage = floatval($this->storage_stage_area->evaluateMatrix($Sn,'stage'));
+		$riser_head = $stage - $this->riser_opening_elev;
+		$riser_emerg_head = $stage - $this->riser_emerg_elev_elev;
+		//error_log("RISER($this->state[runid] : Current stage: $stage, riser_head: $riser_head, Riser Opening S = $this->riser_opening_storage (elev: $this->riser_opening_elev), Current S1 = $S1");
+		//error_log("RISER($this->state[runid] : Riser Head: $riser_head, riser_pipe_flow_head: $this->riser_pipe_flow_head, Riser Opening S = $this->riser_opening_storage");
+		// Now, if max possible riser_head > 0 then we have at least some flow out of riser
+		//error_log("S0 ($S0) + Qin ($Qin) = $S1"); 
+		if ($riser_head > 0 and $riser_emerg_elev < 0) {
+			// determine which orifice equation to use depending on riser_head
+			if ($riser_head > $this->riser_pipe_flow_head) {
+				//error_log("Head = $riser_head - Pipe Flow");
+				// pipe flow
+				$riser_flow = 0.6 * $this->riser_length 
+				* $this->riser_diameter 
+				* sqrt(2.0 * 32.2 * ($riser_head - (0.5 * $this->riser_length)))
+				;
+				$riser_mode = 'pipe';
+			} else {
+				//error_log("Head = $riser_head - Weir Flow");
+				// weir flow 
+				$riser_flow = 3.1 * $this->riser_diameter * pow($riser_head, 1.5);
+				//error_log("$riser_flow = 3.1 * $this->riser_diameter * pow($riser_head, 1.5)");
+				$riser_mode = 'weir';
+			}
+		} elseif ($riser_emerg_elev > 0){
+			$riser_flow = (0.6 * $this->riser_length 
+          * $this->riser_diameter 
+          * sqrt(2.0 * 32.2 * ($riser_head - (0.5 * $this->riser_length))))
+		  + 3.1 * $this->riser_emerg_diameter * pow($riser_emerg_head, 1.5)
+			;
+        $riser_mode = 'Emergency';
+		} else {
+			$riser_flow = 0.0;
+		}
+		//Now that flow has been calculated, the bisection method can be continued. Need to shorten interval with guess Sn
+		//Compute the MPM equation for S1 (maximum storage) and Sn (current iterator). If product is negative, they are of
+		//opposite sign. Thus, a solution for S and Q are contained within this new interval, replace Si with Sn. Otherwise,
+		//if they are of the same sign, assign Sn as S1 to serve as the new maximum storage value. Then replace $riserP with 
+		//the current riser_flow for future reference in solving the MPM for S1
+		if ((($Sn-$S0+$riser_flow*$dt/43560)-($Qin*$dt/43560))*(($S1-$S0+$riserP*$dt/43560)-($Qin*$dt/43560))<0){
+			$Si = $Sn;	
+		} else {
+			$S1 = $Sn;
+			$riserP = $riser_flow
+		}
+      } else {
+        //Tolerance achieved, solution found
+        break;
+      }
+      if($x>500){
+        //To save time and prevent looping, a counter check
+        $this->riser_flow=$Qin;
+        $S1=$S0;
+        break;
+      }
+    }//end loop
     // store this in both places, the 'value' property is assumed for subcomps and others are for state 
     $this->riser_flow = $riser_flow;
     $this->riser_head = $riser_head;
