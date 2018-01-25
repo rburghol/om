a6ad5ee0ccf07ce73fbda31e1c4806a4fb532b2b
rburghol <rburghol@vt.edu>
2018-01-25 08:59:34 -0500

    Update omRuntime_HydroRiser.class.php

--- php/plugins/omRuntime_HydroRiser.class.php
+++ php/plugins/omRuntime_HydroRiser.class.php
@@ -1,12 +1,5 @@
 <?php
 
-class omRuntime_SubComponent {
-  var $value;
-  function __construct($options) {
-    return TRUE;
-  }
-}
-
 class omRuntime_HydroRiser extends omRuntime_SubComponent {
   var $storage_stage_area = FALSE;
   var $riser_opening_elev = FALSE;
