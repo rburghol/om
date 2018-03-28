5abdf37be3bdd851092990bd675e7dc2d8d39c97
connorb5 <connorb5@vt.edu>
2018-03-28 14:38:05 -0400

    Added SA to the SS.csv file to allow for SA interpolation. Corrected MPM2.R to adjust for evap and precip when simulating the MPM

--- analysis/R/examples/MPM2.R
+++ analysis/R/examples/MPM2.R
@@ -56,40 +56,54 @@ dat$its<-numeric(length(dat$impoundment_Qin))
 for (i in 2:length(dat$impoundment_Qin)){
   S0<-dat$MPMStorage[i-1]#Stores previous timestep storage for easy reference
   Qin<-as.numeric(dat$impoundment_Qin[i])#Stores inflow for easy reference
-  S1<-S0+(Qin*3600/43560)#Maximum possible storage
+  ET<-dat$et_in[i]/12/24
+  P<-dat$precip_in[i]/12/24
+  SA<-approx(x=SS$Storage,y=SS$SA,xout=S0,rule=1)$y
+  S1<-S0+(Qin*3600/43560)-(ET_imp-P_imp)#Maximum possible storage
   riser_flow<-Solver(S1)#Maximum possible outflow
   riserP<-riser_flow#Maximum interval outflow for use in bisection method
   Si<-0#Minimuim storage for use in bisection method
   Sn<-S1#A storage to be iterated within the below while loop
+  SA<-approx(x=SS$Storage,y=SS$SA,xout=Sn,rule=1)$y
+  ET_imp<-ET*SA
+  P_imp<-P*SA
   #Begin a loop that continuously computes the MPM equation until tolerance is achieved
   x<-1
-  while (abs((Sn-S0+riser_flow*dt/43560)-(Qin*dt/43560)) > 0.0001){
-    x<-x+1
-    #Check the conditional statement in the while loop to break the loop before computation
-    if (x>500){
-      Sn<-S0
-      riser_flow<-Qin
-      break
-    }
-    diff<-abs((Sn-S0+riser_flow*dt/43560)-(Qin*dt/43560))
-    if (abs((Sn-S0+riser_flow*dt/43560)-(Qin*dt/43560)) > 0.0001){
-      #If tolerance has not been achieved, use the bisection method to find S and Q
-      Sn<-(S1+Si)/2#New storage computed from the midpoint of max and min storage, S1 and Si respectivley
-      riser_flow<-Solver(Sn)#Corresponding outflow
-      #Now that flow has been calculated, the bisection method can be continued. Need to shorten interval with guess Sn
-      #Compute the MPM equation for S1 (maximum storage) and Sn (current iterator). If product is negative, they are of
-      #opposite sign. Thus, a solution for S and Q are contained within this new interval, replace Si with Sn. Otherwise,
-      #if they are of the same sign, assign Sn as S1 to serve as the new maximum storage value. Then replace riserP with 
-      #the current riser_flow for future reference in solving the MPM for S1
-      if(((Sn-S0+riser_flow*dt/43560)-(Qin*dt/43560))*((S1-S0+riserP*dt/43560)-(Qin*dt/43560))<0){
-        Si<-Sn
-      } else {
-        S1<-Sn
-        riserP<-riser_flow
+  if(riser_flow>0){
+    while (abs((Sn-S0+(ET_imp-P_imp)+riser_flow*dt/43560)-(Qin*dt/43560)) > 0.0001){
+      x<-x+1
+      #Check the conditional statement in the while loop to break the loop before computation
+      if (x>500){
+        Sn<-S0-(ET_imp-P_imp)
+        riser_flow<-Qin
+        break
       }
-    } else {
-      #Tolerance achieved, solution found
-      break
+      diff<-abs((Sn-S0+(ET_imp-P_imp)+riser_flow*dt/43560)-(Qin*dt/43560))
+      if (abs((Sn-S0+(ET_imp-P_imp)+riser_flow*dt/43560)-(Qin*dt/43560)) > 0.0001){
+        #If tolerance has not been achieved, use the bisection method to find S and Q
+        Sn<-(S1+Si)/2#New storage computed from the midpoint of max and min storage, S1 and Si respectivley
+        riser_flow<-Solver(Sn)#Corresponding outflow
+        SA<-approx(x=SS$Storage,y=SS$SA,xout=Sn,rule=1)$y
+        ET_imp<-ET*SA
+        P_imp<-P*SA
+        #Now that flow has been calculated, the bisection method can be continued. Need to shorten interval with guess Sn
+        #Compute the MPM equation for S1 (maximum storage) and Sn (current iterator). If product is negative, they are of
+        #opposite sign. Thus, a solution for S and Q are contained within this new interval, replace Si with Sn. Otherwise,
+        #if they are of the same sign, assign Sn as S1 to serve as the new maximum storage value. Then replace riserP with 
+        #the current riser_flow for future reference in solving the MPM for S1
+        SA1<-approx(x=SS$Storage,y=SS$SA,xout=S1,rule=1)$y
+        ET_imp1<-ET*SA1
+        P_imp1<-P*SA1
+        if(((Sn-S0+(ET_imp-P_imp)+riser_flow*dt/43560)-(Qin*dt/43560))*((S1-S0+(ET_imp1-P_imp1)+riserP*dt/43560)-(Qin*dt/43560))<0){
+          Si<-Sn
+        } else {
+          S1<-Sn
+          riserP<-riser_flow
+        }
+      } else {
+        #Tolerance achieved, solution found
+        break
+      }
     }
   }
   #Store stage, storage, and outflow calculated from the MPM method for plotting
@@ -120,11 +134,11 @@ lines(
   lwd=2,
   type='l'
 )
-points(as.POSIXct(dat$timestamp[dat$impoundment_its>500&dat$impoundment_riser_head>0]),dat$impoundment_lake_elev[dat$impoundment_its>500&dat$impoundment_riser_head>0])
+points(as.POSIXct(dat$timestamp[dat$impoundment_its>500&dat$impoundment_riser_head>0]),dat$impoundment_lake_elev[dat$impoundment_its>500&dat$impoundment_riser_head>0],pch=19,cex=3)
 par(new=T)
 plot(dat$timestamp,dat$impoundment_Qin,type='l',lty=3,ann=F,axes=F)
 
-legend(x=5250,y=9.25,c('MPM','VA Hydro'),col=c('blue','red'),lwd=2,pch=1,cex=2,bty='n',y.intersp = 0.5)
+legend('top',c('MPM stage','VA Hydro stage','Inflow'),col=c('blue','red','black'),lty=c(1,1,3),lwd=4,cex=2,bty='n',y.intersp = 0.5,x.intersp = 0.5,seg.len=0.75)
 
 
 
