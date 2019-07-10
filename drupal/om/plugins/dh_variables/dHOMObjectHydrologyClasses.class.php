2c0069b14c9564192e8b739e86b39595707231a3
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2019-07-10 13:30:08 -0400

    Turned on debug for impoundment.

--- drupal/om/plugins/dh_variables/dHOMObjectHydrologyClasses.class.php
+++ drupal/om/plugins/dh_variables/dHOMObjectHydrologyClasses.class.php
@@ -10,6 +10,7 @@ $class = ctools_plugin_get_class($plugin_def, 'handler');
 class dHOMHydroImpoundment extends dHOMModelElement {
   var $object_class = 'hydroImpoundment';
   var $attach_method = 'contained';
+  var $debug = TRUE;
   
   public function hiddenFields() {
     $hidden = array_merge(array('propcode', 'propvalue'), parent::hiddenFields());
