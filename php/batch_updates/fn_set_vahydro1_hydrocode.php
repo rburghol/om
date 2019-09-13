29c89f66c3bff85386dfd1f1dd9604200cffe93b
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2019-09-13 10:44:03 -0400

    Added riverseg

--- php/batch_updates/fn_set_vahydro1_hydrocode.php
+++ php/batch_updates/fn_set_vahydro1_hydrocode.php
@@ -18,18 +18,19 @@ error_log(print_r($argv,1));
 $riverseg = $argv[1];
 
 if ($riverseg == 'all') {
-  $listobject->querystring = "select elementid from scen_model_element where custom1 = 'cova_ws_container' and scenarioid = $scenarioid ";
+  $listobject->querystring = "select elementid, custom2 from scen_model_element where custom1 = 'cova_ws_container' and scenarioid = $scenarioid ";
   $listobject->performQuery();
   $rsegs = $listobject->queryrecords;
 } else {
   $elid = getCOVACBPContainer($listobject, $scenarioid, $riverseg);
   $rsegs = array(
-    0 => array( 'elementid' => $elid )
+    0 => array( 'elementid' => $elid, 'custom2' => $riverseg )
   );
 }
 
 foreach ($rsegs as $seg) {
   $elid = $seg['elementid'];
+  $riveseg = $seg['custom2'];
   $wds = getCOVAWithdrawals($listobject, $elid);
   
   foreach ($wds as $thiswd) {
@@ -37,7 +38,7 @@ foreach ($rsegs as $seg) {
     $loadres = unSerializeSingleModelObject($wd_elid);
     $wdobject = $loadres['object'];
     $hydrocode = $wdobject->id1;
-    $q = "update scen_model_element set hydrocode = '$hydrocode' where elementid = $wd_elid";
+    $q = "update scen_model_element set hydrocode = '$hydrocode', riverseg = '$riverseg' where elementid = $wd_elid";
     $listobject->querystring = $q;
     error_log($q);
     $listobject->performQuery();
