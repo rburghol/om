5f7422ac08bf17aeedaa495f06d64c655456ac60
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2019-09-20 09:41:02 -0400

    Put back on save() -- will have to only call this pull after initial save so as to percvent redundant

--- drupal/om/plugins/dh_variables/dHOMObjectClasses.class.php
+++ drupal/om/plugins/dh_variables/dHOMObjectClasses.class.php
@@ -919,20 +919,12 @@ class dHOMElementConnect extends dHOMBaseObjectClass {
     );
     
   }
-  public function update(&$entity) {
-    parent::update($entity);
+  public function save(&$entity) {
+    parent::save($entity);
     if ($entity->propcode == 'pull_once') {
       // pull from remote, then set this back to previous entity value 
       $this->pullFromRemote($entity);
-      $entity->propcode = '1';
-    }
-  }
-  
-  public function insert(&$entity) {
-    parent::insert($entity);
-    if ($entity->propcode == 'pull_once') {
-      // pull from remote, then set this back to previous entity value 
-      $this->pullFromRemote($entity);
+      // @todo: because the entity is already updatred by the time we get here, we can't retrieve the previous synch setting, so we assume that it is OK to push remote changes after this save and poull is complete.  Why?  Can't we intercept before entity is updated?
       $entity->propcode = '1';
     }
   }
