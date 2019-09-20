c2396922da037f104f5975b32480ca91c9649f00
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2019-09-20 09:03:28 -0400

    Use DRUPAL_ROOT

--- drupal/om/plugins/dh_variables/dHOMObjectClasses.class.php
+++ drupal/om/plugins/dh_variables/dHOMObjectClasses.class.php
@@ -931,7 +931,7 @@ class dHOMElementConnect extends dHOMBaseObjectClass {
   
   public function pullFromRemote($entity) {
     global $base_url;
-    $cmd = "cd " . $base_url . drupal_get_path('module', 'om') . "/src/ \n";
+    $cmd = "cd " . DRUPAL_ROOT . drupal_get_path('module', 'om') . "/src/ \n";
     $cmd .= "drush om.migrate.element.php pid $entity->propvalue $entity->featureid ";
     dpm( $cmd, "Executing ");
     //shell_exec($cmd);
