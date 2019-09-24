7c104a10891ad04f3285bc2422cf91e99c56b3a3
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2019-09-24 16:13:47 -0400

    Test query for rseg

--- drupal/om/src/om.migrate.wd.php
+++ drupal/om/src/om.migrate.wd.php
@@ -104,27 +104,43 @@ foreach ($data as $element) {
   $propvalue = isset($element['propvalue']) ? $element['propvalue'] : FALSE;
   $propname = isset($element['propname']) ? $element['propname'] : FALSE;
   // add a default var class for files that come in without one.
-  if (!$prop_varkey and $propname) {
-    $prop_varkey = 'om_class_Equation';
+  
+  // Now, search for this riverseg in any existing models 
+  $dh_model_pid = FALSE;
+  $rseg_pid = om_get_search_model_subprops('dh_feature', $feature_hydroid, 'riverseg', TRUE, $riverseg, $model_scenario);
+  if ($rseg_pid) {
+    error_log("Found $rseg_pid");
+    // now that pid parent is the model in question
+    $riverseg_prop = entity_load_single('dh_properties', $rseg_pid);
+    $dh_model_pid = $riverseg_prop->featureid;
+    error_log("Found Matching model: $dh_model_pid");
   }
-  // add a new model if one does not exist - propname match 
-  // add a riverseg prop to model 
-  // If requested, add another equation prop 
-  $values = array(
-    'varkey' => 'om_water_system_element', 
-    'propname' => $feature_name . ':' . $coverage_name,
-    'featureid' => $feature_hydroid,
-    'propvalue' => NULL,
-    'propcode' => 'vahydro-1.0', 
-    'entity_type' => 'dh_feature',
-  );
-  error_log("Adding: " . $feature_name . ':' . $coverage_name . " to " . $feature_hydroid);
-  if ($debug) error_log("Values: " . print_r($values,1));
-  // alternative: query the riverseg propcodes on all model children and select the matching one
-  // so all models: featureid = $feature_hydroid and propcode = vahydro-1.0$riverseg
-  // Then, get all children properties with name "riverseg" and propcode = $riverseg 
-  $dh_model = om_model_getSetProperty($values, 'name', FALSE);
-  $dh_model->riverseg = $riverseg;
+  if (!dh_model_pid) {
+    if (!$prop_varkey and $propname) {
+      $prop_varkey = 'om_class_Equation';
+    }
+    // add a new model if one does not exist - propname match 
+    // add a riverseg prop to model 
+    // If requested, add another equation prop 
+    $values = array(
+      'varkey' => 'om_water_system_element', 
+      'propname' => $feature_name . ':' . $coverage_name,
+      'featureid' => $feature_hydroid,
+      'propvalue' => NULL,
+      'propcode' => 'vahydro-1.0', 
+      'entity_type' => 'dh_feature',
+    );
+    error_log("Adding: " . $feature_name . ':' . $coverage_name . " to " . $feature_hydroid);
+    if ($debug) error_log("Values: " . print_r($values,1));
+    // alternative: query the riverseg propcodes on all model children and select the matching one
+    // so all models: featureid = $feature_hydroid and propcode = vahydro-1.0$riverseg
+    // Then, get all children properties with name "riverseg" and propcode = $riverseg 
+    $dh_model = om_model_getSetProperty($values, 'name', FALSE);
+    $dh_model->riverseg = $riverseg;
+  } else {
+    $dh_model = entity_load_single('dh_properties', $dh_model_pid);
+    $dh_model->propname = $feature_name . ':' . $coverage_name;
+  }
   $dh_model->save();
   
   if ($prop_varkey) {
--- drupal/om/src/test_search.php
+++ drupal/om/src/test_search.php
@@ -6,4 +6,5 @@ module_load_include('inc', 'om', 'src/om_translate_to_dh');
 
 $pid = om_get_search_model_subprops('dh_feature', 67292, 'riverseg', TRUE, 'YM3_6430_6620', 'vahydro-1.0');
 error_log("Found $pid");
+// now that pid parent is the model in question
 ?>
