c3abcf8300b565d98a6221fd9ef4bca1ac5423d9
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2019-09-24 13:40:28 -0400

    Debug cleanup

--- drupal/om/plugins/dh_variables/dHOMObjectClasses.class.php
+++ drupal/om/plugins/dh_variables/dHOMObjectClasses.class.php
@@ -584,7 +584,7 @@ class dHOMBaseObjectClass extends dHVariablePluginDefaultOM {
         'varid' => dh_varkey2varid('om_object_class', TRUE),
       );
       dh_update_properties($values, 'singular');
-      error_log("Saving object_class: " . print_r($values,1));
+      //error_log("Saving object_class: " . print_r($values,1));
     }
   }
   public function getDefaults($entity, &$defaults = array()) {
