10ed807ad02f63c1b1adf5940a233cad18e68382
Burgholzer <robert.burgholzer@deq.virginia.gov>
2019-10-04 10:29:03 -0400

    debug

--- drupal/om/src/om_edit_matrix_runit.php
+++ drupal/om/src/om_edit_matrix_runit.php
@@ -58,7 +58,10 @@ foreach ($data as $element) {
   }
   //$dh_prop = om_load_dh_model('pid', $vahydro_pid);
   $dh_prop = entity_load_single('dh_properties', $vahydro_pid);
-  error_log("Props and Fields: " . print_r(get_object_vars ($dh_prop),1));
+  $fields = get_object_vars ($dh_prop);
+  foreach ($fields as $fname) {
+	  error_log("$fname" );
+  }
   error_log("propname: " . $dh_prop->propname);
   error_log("pid: " . $dh_prop->pid);
   error_log("Varid: " . $dh_prop->varid);
