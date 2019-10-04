10a5d4fb7b0c38453e6894271420d136e02604a3
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2019-10-04 09:59:17 -0400

    debug

--- drupal/om/src/om_edit_matrix_runit.php
+++ drupal/om/src/om_edit_matrix_runit.php
@@ -57,13 +57,13 @@ foreach ($data as $element) {
     die;
   }
   $dh_prop = om_load_dh_model('pid', $vahydro_pid);
-  $plugin = dh_variables_getPlugins($dh_prop);
   error_log("Props and Fields: " . print_r(array_keys((array)$dh_prop),1));
   error_log("propname: " . $dh_prop->propname);
   error_log("pid: " . $dh_prop->pid);
   error_log("Varid: " . $dh_prop->varid);
   error_log("bundle: " . $dh_prop->bundle);
   error_log("Varkey: " . $dh_prop->varkey);
+  $plugin = dh_variables_getPlugins($dh_prop);
   $om_matrix = $plugin->tablefieldToOMMatrix($dh_prop->field_dh_matrix);
   error_log("Matrix: " . print_r($om_matrix,1));
   /*
