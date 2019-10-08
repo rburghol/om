3463e8c2fd185302db29e779ad65bc81bf278cec
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2019-10-08 11:19:04 -0400

    Updated setup.sh

--- sh/setup.sh
+++ sh/setup.sh
@@ -2,9 +2,11 @@
 # Set up scripts
 # dev
 ln -s /opt/model/om-dev/php/src/fn_copy_element.php /var/www/html/om/fn_copy_element.php
+ln -s /opt/model/om-dev/php/src/fn_copy_group_subcomp.php /var/www/html/om/fn_copy_group_subcomp.php
 ln -s /opt/model/om-dev/php/src/fn_batchedit_broadcast_matrix.php /var/www/html/om/fn_batchedit_broadcast_matrix.php
 ln -s /opt/model/om-dev/php/lib/lib_hydrology.php /var/www/html/lib/lib_hydrology.php
 # live
 ln -s /opt/model/om/php/src/fn_copy_element.php /var/www/html/om/fn_copy_element.php
+ln -s /opt/model/om/php/src/fn_copy_group_subcomp.php /var/www/html/om/fn_copy_group_subcomp.php
 ln -s /opt/model/om/php/src/fn_batchedit_broadcast_matrix.php /var/www/html/om/fn_batchedit_broadcast_matrix.php
 ln -s /opt/model/om/php/lib/lib_hydrology.php /var/www/html/lib/lib_hydrology.php
