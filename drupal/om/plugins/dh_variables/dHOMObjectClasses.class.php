a5238e443057c2432e2ab80e0acef828adc0948e
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2019-10-10 16:36:03 -0400

    publix luyear

--- drupal/om/plugins/dh_variables/dHOMObjectClasses.class.php
+++ drupal/om/plugins/dh_variables/dHOMObjectClasses.class.php
@@ -1384,14 +1384,12 @@ class dHOMDataMatrix extends dHOMSubComp {
   
   public function getPublicVars($entity, &$publix = array()) {
     // @todo: if this works, move to the dHOMSubComp class
-    dsm("getPublicVars called on $entity->propname");
-    return;
     $parent = $this->getParentEntity($entity);
     $plugin = dh_variables_getPlugins($parent);
     if ($plugin) {
     //dpm($plugin,'plugin');
       if (method_exists($plugin, 'getPublicVars')) {
-        $plugin->getPublicVars($entity, $publix);
+        $plugin->getPublicVars($parent, $publix);
       }
     }
     return $publix;
