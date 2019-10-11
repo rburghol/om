42e189943fed32a1a9286fcdd5b5b39a26219d0c
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2019-10-11 08:15:18 -0400

    Finally override the runoff object file if flow_scenario property exists

--- php/lib/lib_wooomm.cbp.php
+++ php/lib/lib_wooomm.cbp.php
@@ -642,9 +642,10 @@ class CBPLandDataConnectionFile extends timeSeriesFile {
       $this->processors['flow_scenario']->debug = 1;
       $flow_scenario = $this->processors['flow_scenario']->evaluateMatrix($flow_mode);
       error_log("flow_scenario prop found: $flow_scenario ");
-      error_log("State array:" . print_r($this->state,1));
-      $this->processors['flow_scenario']->debug = $dbc;
-      $retfile = $this->filepath;
+      //error_log("State array:" . print_r($this->state,1));
+      $this->scenario = $flow_scenario;
+      //$this->processors['flow_scenario']->debug = $dbc;
+      $retfile = implode("/", array($this->modelpath, 'out/land', $this->scenario, 'eos', $this->landseg .'_0111-0211-0411.csv' ));
     } else {
       $retfile = $this->filepath;
     }
