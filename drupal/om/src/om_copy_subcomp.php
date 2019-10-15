07281d90c02b792e9f2b08896db2a3b831dc45e7
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2019-10-15 10:05:28 -0400

    parse props

--- drupal/om/src/om_copy_subcomp.php
+++ drupal/om/src/om_copy_subcomp.php
@@ -70,8 +70,8 @@ foreach ($data as $element) {
     'propname' => $element['propname'],
     'featureid' => $element['src_id']
   );
-  $src_prop = om_get_property((array)$src_prop, 'name');
-  error_log("Info:" . print_r($copyable));
+  $src_prop = om_get_property($values, 'name');
+  error_log("Info:" . print_r((array)$src_prop));
   $copy_values = array();
   $copyable = $src_prop->entityInfo();
   error_log("Info:" . print_r($copyable));
