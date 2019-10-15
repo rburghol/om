baa2c99ddbeb084cc570b8f73a11e09fdb448de9
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2019-10-15 12:23:41 -0400

    Handle textfield version of flow_scenario

--- php/lib/lib_wooomm.cbp.php
+++ php/lib/lib_wooomm.cbp.php
@@ -638,7 +638,11 @@ class CBPLandDataConnectionFile extends timeSeriesFile {
     // if component flow_scenario is set, 
     // then override the scenario setting and the file name 
     if (isset($this->processors['flow_scenario'])) {
-      $flow_scenario = $this->processors['flow_scenario']->evaluateMatrix($flow_mode);
+      if (method_exists($this->processors['flow_scenario'], 'evaluateMatrix')) {
+        $flow_scenario = $this->processors['flow_scenario']->evaluateMatrix($flow_mode);
+      } else {
+        $flow_scenario = $this->getProp('flow_scenario');
+      }
       //error_log("flow_scenario prop found: $flow_scenario ");
       //error_log("State array:" . print_r($this->state,1));
       $this->scenario = $flow_scenario;
