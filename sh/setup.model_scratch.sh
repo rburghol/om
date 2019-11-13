17b0257b37f10fd77a081981e038dfedb6591e6e
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2019-11-13 10:10:35 -0500

    Use variables for port on model_scratch init script

--- sh/setup.model_scratch.sh
+++ sh/setup.model_scratch.sh
@@ -2,6 +2,7 @@
 # shell
 pgpath="/data/postgres/9.5"
 bindir="/usr/lib/postgresql/9.5/bin"
+db_port=5433
 
 mkdir $pgpath/scratch
 $bindir/initdb -D $pgpath/scratch
@@ -13,5 +14,5 @@ nano $pgpath/scratch/postgresql.conf
 # start it up
 $bindir/pg_ctl -D $pgpath/scratch start -l "logfile.scratch"
 
-$bindir/createdb model_scratch -p 5444
-echo "CREATE EXTENSION postgis;" | psql model_scratch -p 5444
+$bindir/createdb model_scratch -p $db_port
+echo "CREATE EXTENSION postgis;" | psql model_scratch -p $db_port
