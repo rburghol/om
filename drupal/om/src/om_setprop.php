154388fa44e567af5b1df84261b7bd814ea72a1c
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2019-11-15 18:07:23 -0500

    Started adding setprop script

--- drupal/om/src/om_setprop.php
+++ drupal/om/src/om_setprop.php
@@ -9,37 +9,55 @@ while ($arg = drush_shift()) {
 
 // Is single command line arg?
 $vars = array();
-if (count($args) >= 6) {
-  $query_type = $args[0];
-  $vars['entity_type'] = $args[1];
-  $vars['featureid'] = $args[2];
-  $vars['pid']= $args[3];
-  $vars['propname'] = $args[4];
-  $vars['propvalue'] = $args[5];
-  $vars['propcode'] = $args[6];
-} else {
+
+
+if (count($args) < 1) {
   error_log("Usage: php om_setprop.php query_type entity_type featureid pid propname propvalue propcode ");
   error_log("Note: 'all' is not yet enabled");
   die;
 }
-
-if ($query_type <> 'cmd') {
-  error_log("Only cmd mode enabled");
-  die;
+$query_type = $args[0];
+$data = array();
+if ($query_type == 'cmd') {
+  if (count($args) >= 6) {
+    $vars = array();
+    $vars['entity_type'] = $args[1];
+    $vars['featureid'] = $args[2];
+    $vars['pid']= $args[3];
+    $vars['propname'] = $args[4];
+    $vars['propvalue'] = $args[5];
+    $vars['propcode'] = $args[6];
+    $data[] = $vars;
+  } else {
+    error_log("Usage: php om_setprop.php query_type entity_type featureid pid propname propvalue propcode ");
+    error_log("Note: 'all' is not yet enabled");
+    die;
+  }
+} else {
+  $filepath = $dest_id;
+  $dest_id = FALSE;
+  $src_id = FALSE;
+  error_log("File requested: $filepath");
+  $file = fopen($filepath, 'r');
+  $header = fgetcsv($file, 0, "\t");
+  if (count($header) == 0) {
+    $header = fgetcsv($file, 0, "\t");
+  }
+  while ($line = fgetcsv($file, 0, "\t")) {
+    $data[] = array_combine($header,$line);
+  }
+  error_log("File opened with records: " . count($data));
+  error_log("Header: " . print_r($header,1));
+  error_log("Record 1: " . print_r($data[0],1));
 }
 
-$q = "select pid from {dh_properties}  ";
-$q .= " where propname = :propname "
-$q .= " and entity_type = :entity_type "
-if ($vars['pid'] <> 'all') {
-  $q .= " and entity_type = :pid ";
-  $vars['pid'] = $pid;
-} 
+
+
+foreach ($data as $element) {
 
 
 
-
-
+}
 
 
 
