485a3ddd593e7cf081ebdb2c510d048c5f5d5c84
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2019-11-20 06:49:03 -0500

    draft multi version runpof harvesting code

--- sh/create_landseg_table.sh
+++ sh/create_landseg_table.sh
@@ -2,17 +2,22 @@
 
 
 if [ $# -lt 2 ]; then
-  echo 1>&2 "Usage: create_landuse_table.sh N51045 CFBASE30Y20180615"
+  echo 1>&2 "Usage: create_landuse_table.sh N51045 CFBASE30Y20180615 [version=p6]"
   exit 2
 fi 
 
 landseg=$1
 scenario=$2
+if [ $# -gt 2 ]; then
+  version=$3
+else
+  version="p6"
+fi
 
 # i.e. create_landseg_table.sh N51045 JU1_7630_7490 CFBASE30Y20180615
-template="cbp_p6_cfbase30y20180615_n51161"
-filename="/media/NAS/omdata/p6/out/land/$scenario/eos/${landseg}_0111-0211-0411.csv"
-tablename="cbp_p6_${scenario}_${landseg}"
+template="cbp_${version}_cfbase30y20180615_n51161"
+filename="/media/NAS/omdata/${version}/out/land/$scenario/eos/${landseg}_0111-0211-0411.csv"
+tablename="cbp_${version}_${scenario}_${landseg}"
 tablename=`echo $tablename | tr '[:upper:]' '[:lower:]'`
 hdrcols=`head -n 1 $filename`
 
