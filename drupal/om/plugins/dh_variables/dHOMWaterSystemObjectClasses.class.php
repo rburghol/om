fefc9ef26117b0851faeb6238df6a138e686c5a3
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2019-12-03 09:09:38 -0500

    CFB variable tiered table defaults

--- drupal/om/plugins/dh_variables/dHOMWaterSystemObjectClasses.class.php
+++ drupal/om/plugins/dh_variables/dHOMWaterSystemObjectClasses.class.php
@@ -145,6 +145,21 @@ class dHOMWaterSystemObject extends dHOMModelElement {
   }
 }
 
+// ## Special function for use by tiered and simple flowby vars
+public function formatCFB(&$form, $entity) {
+  // combine into fieldset
+  $form['cfb'] = array(
+    '#type' => 'fieldset',
+    '#weight' => -5,
+  );
+  $form['cfb']['enable_cfb'] = $form['enable_cfb'];
+  $form['cfb']['cfb_condition'] = $form['cfb_condition'];
+  $form['cfb']['cfb_var'] = $form['cfb_var'];
+  unset($form['cfb_var']);
+  unset($form['cfb_condition']);
+  unset($form['enable_cfb']);
+}
+
 class dHOMWaterSystemFlowBy extends dHOMSubComp {
   var $object_class = 'wsp_flowby';
   var $attach_method = 'contained';
@@ -226,19 +241,7 @@ class dHOMWaterSystemFlowBy extends dHOMSubComp {
     $form['enable_cfb']['#weight'] = -5;
     $form['cfb_condition']['#weight'] = -4;
     $form['cfb_var']['#weight'] = -3;
-    
-    // combine into fieldset
-    
-    $form['cfb'] = array(
-      '#type' => 'fieldset',
-      '#weight' => -5,
-    );
-    $form['cfb']['enable_cfb'] = $form['enable_cfb'];
-    $form['cfb']['cfb_condition'] = $form['cfb_condition'];
-    $form['cfb']['cfb_var'] = $form['cfb_var'];
-    unset($form['cfb_var']);
-    unset($form['cfb_condition']);
-    unset($form['enable_cfb']);
+    $this->formatCFB($form, $entity);
   }
   
 }
@@ -321,6 +324,7 @@ class dHOMWaterSystemTieredFlowBy extends dHOMDataMatrix {
     $form['enable_cfb']['#weight'] = -5;
     $form['cfb_condition']['#weight'] = -4;
     $form['cfb_var']['#weight'] = -3;
+    $this->formatCFB($form, $entity);
   }
   
   public function tableDefault($entity) {
