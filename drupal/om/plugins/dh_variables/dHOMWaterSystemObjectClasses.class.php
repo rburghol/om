54198a8d3e3b31579c7d707c4519c7637f91f01e
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2019-12-04 16:53:46 -0500

    Fix insureProperty siyntax

--- drupal/om/plugins/dh_variables/dHOMWaterSystemObjectClasses.class.php
+++ drupal/om/plugins/dh_variables/dHOMWaterSystemObjectClasses.class.php
@@ -189,21 +189,21 @@ class dHOMWaterSystemObject extends dHOMModelElement {
   public function insureProperty($entity, $thisvar) {
     $prop = parent::insureProperty($entity, $thisvar);
     $defaults = $this->getDefaults($entity);
-    if ($thisvar == 'ps_enabled') {
+    if ($thisvar['propname'] == 'ps_enabled') {
       dpm($entity,'is_new setting defaults insureProperty called');
-      if (is_object($entity->ps_enabled) and $entity->ps_enabled->is_new) {
-        $plugin = dh_variables_getPlugins($entity->ps_enabled);
+      if (is_object($prop) and $prop->is_new) {
+        $plugin = dh_variables_getPlugins($prop);
         if (isset($defaults['ps_enabled']['field_dh_matrix_default'])) {
-          $plugin->setCSVTableField($entity->ps_enabled, $defaults['ps_enabled']['field_dh_matrix_default']);
+          $plugin->setCSVTableField($prop, $defaults['ps_enabled']['field_dh_matrix_default']);
         }
         if (isset($defaults['ps_enabled']['lutype1_default'])) {
-          $entity->ps_enabled->lutype1 = $defaults['ps_enabled']['lutype1_default'];
+          $prop->lutype1 = $defaults['ps_enabled']['lutype1_default'];
         }
         if (isset($defaults['ps_enabled']['keycol1_default'])) {
-          $entity->ps_enabled->keycol1 = $defaults['ps_enabled']['keycol1_default'];
+          $prop->keycol1 = $defaults['ps_enabled']['keycol1_default'];
         }
-        dpm($entity->ps_enabled,'ps_enable');
-        $entity->ps_enabled->save();
+        dpm($prop,'ps_enable');
+        $prop->save();
       }
     }
     return $prop;
