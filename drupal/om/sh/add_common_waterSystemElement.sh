0964c71a4e0d48338ceed6d9e8a014814cdb31db
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2019-12-05 09:40:54 -0500

    Added bash script add_common_waterSystemElement.sh

--- drupal/om/sh/add_common_waterSystemElement.sh
+++ drupal/om/sh/add_common_waterSystemElement.sh
@@ -2,7 +2,9 @@
 
 #filename='/var/www/html/files/vahydro/facs-copy-template.txt'
 #filename='/var/www/html/files/vahydro/facs-copy-template-short.txt'
-filename='/var/www/html/files/vahydro/facs-copy-template-remainder.txt'
+#filename='/var/www/html/files/vahydro/facs-copy-template-remain.txt'
+
+filename=$1
 for i in `cat $filename`; do
   drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties 4988636 dh_properties $i ps_enabled;
   drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties 4988636 dh_properties $i fac_demand_mgy;   
