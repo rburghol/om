eae794a255e8a0f1cd71d1def7d7b417cb7040ea
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2019-12-13 09:23:27 -0500

    debug

--- php/sh/batch_archive_runoffs.sh
+++ php/sh/batch_archive_runoffs.sh
@@ -6,6 +6,6 @@ fi
 filename="$1"
 while IFS= read -r line; do
     echo "Text read from file: $line"
-    #read -r riverseg parentid oldelement src_file <<< "$line"
+    read -r riverseg parentid oldelement src_file <<< "$line"
     #echo "archive_runoff.sh $riverseg $parentid $oldelement $src_file"
 done < $filename 
