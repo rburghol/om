66bd8059b952361c020f7938425d6d4393b1a0d0
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2019-12-16 05:43:24 -0500

    Allowed date args in runoff batches

--- drupal/om/sh/run_runoffs.sh
+++ drupal/om/sh/run_runoffs.sh
@@ -10,8 +10,17 @@ run_mode=$2
 flow_mode=$3
 force=$4
 startdate="1984-01-01"
+if [ $# -gt 4 ]; then
+  startdate=$5
+fi 
 enddate="2014-12-31"
+if [ $# -gt 5 ]; then
+  enddate=$6
+fi 
 cachedate="2019-12-01"
+if [ $# -gt 6 ]; then
+  cachedate=$7
+fi 
 # run all runoff containers 
 q="select b.propvalue "
 q="$q from dh_properties as a "
