9149c423ea16e8afa4706ed6740d8aa610c50cca
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2019-12-17 15:40:57 -0500

    Archive point source update

--- php/sh/archive_pointsource.sh
+++ php/sh/archive_pointsource.sh
@@ -1,6 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 # Rename, move, replace Point Source Folders with a timeseries cache of old model runs
+templateid=347395
+archive=347396
+newscen=30
+basedir="/media/NAS/omdata"
 
 # Inputs:
 #   - riverseg ID
@@ -11,10 +15,11 @@
 riverseg=$1
 parentid=$2
 oldelement=$3
-templateid=347395
-archive=347396
-newscen=30
-basedir="/media/NAS/omdata"
+srcfile=$4
+if [ $# -lt 4 ]; then
+  echo 1>&2 "Usage: archive_runoff.sh riverseg parentid oldelement srcfile [template$template]"
+  exit 2
+fi 
 
 # create a clone of the new element desired
 cd /var/www/html/om 
@@ -26,8 +31,7 @@ php fn_addObjectLink.php $oldelement $archive
 newid=`php fn_copy_element.php 37 $templateid $parentid`
 # set the "remote_url" property, even though this is not remote, 
 # it allows us to set a path and avoid filenice browser which should be deprecated anyhow.
-newfilename="$basedir/p6/vahydro/point_source/ps.${riverseg}.2010.2.log"
-srcfile="$basedir/proj3/out/runlog2.${oldelement}.log"
+newfilename="$basedir/p6/vahydro/point_source/ps.${riverseg}.2010.vahydro.log"
 
 sudo cp $srcfile $newfilename 
 php setprop.php $newid name="PS TimeSeries:$riverseg"
---
+++ php/sh/best_ps_archives.sql
@@ -0,0 +1,26 @@
+select a.custom2 as riverseg,
+  c.elementid as parentid, 
+  d.elementid as oldelement, 
+  rf.src_file
+from scen_model_element as a 
+-- grab the WS/PS container
+left outer join map_model_linkages as l1   
+on (
+  a.elementid = l1.dest_id 
+  and l1.linktype = 1
+) 
+-- Grab the PS sub-container
+left outer join scen_model_element as c  
+on (c.elementid = l1.src_id)  
+left outer join map_model_linkages as l2   
+on (
+  c.elementid = l2.dest_id 
+  and l2.linktype = 1
+) 
+left outer join tmp_runoff_archives as rf
+on (rf.elementid = d.elementid)
+where c.custom1 = 'cova_ps_group' 
+  and a.scenarioid = 37 
+  and d.elementid is not null 
+  and src_file is not null
+;
