1b9732154f1ed603f81e9fe8bdbf47c7c634b522
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2019-12-18 17:47:40 -0500

    prototype add water element script

--- drupal/om/sh/add_waterSystemElement.sh
+++ drupal/om/sh/add_waterSystemElement.sh
@@ -9,7 +9,7 @@ src_propname='Water Supply Model Element Template'
 om_parent=-1
 om_template_id=340402
 
-if [ $# -lt 2 ]; then
+if [ $# -lt 3 ]; then
   echo 1>&2 "Usage: add_waterSystemElement.sh entity_type entity_id system_name [om_parent=NULL] [src_entity_type] [src_entity_id] [src_propname]"
   exit 2
 fi 
@@ -26,22 +26,17 @@ fi
 
 # create the element
 drush scr modules/om/src/om_copy_subcomp.php cmd $src_entity_type $src_entity_id $entity_type $entity_id "$src_propname|$system_name"
+pid=`drush scr modules/om/src/om_getpid.php $entity_type $entity_id "$system_name"`
+
 
 # create an om_element_connection, with nothing
 # modify element connection to have parent submitted
 # call push always 
 
 if [ $om_parent -gt 0 ]; then
-# @todo: need to have a custom php script to set these linkages and synch
-# om_element_connection has all the guts needed to do this. 
-# Load model element property as $prop 
-# Load om_element_conection prop as $conprop 
-# set $conprop->propcode = 'clone'
-# set $conprop->om_template_id 
-# set $conprop->remote_parentid
-# $conprop->save()
-# Then, save property
-# $prop->save()
-  drush scr modules/om/src/om_setprop.php cmd dh_properties 4725405 om_element_connection om_element_connection NULL clone om_template_id=$om_template_id&remote_parentid=$om_parent
+# create a remote om element below the indicated parent 
+# and then push the changes from the vahydro entity to the remote OM record
+  drush scr modules/om/src/om_setprop.php cmd dh_properties $pid om_element_connection om_element_connection NULL clone om_template_id=$om_template_id&remote_parentid=$om_parent
+  drush scr modules/om/src/om_saveprop.php cmd $entity_type $entity_id "$system_name"
   
 fi 
