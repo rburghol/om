4914d17851fe6d08eae3d63d925b2e2902147e84
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2019-12-19 08:03:17 -0500

    Added omify_waterSystemElement.sh

---
+++ drupal/om/sh/omify_waterSystemElement.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+entity_type=$1
+entity_id=$2
+system_name=$3
+vahydro_model_pid=$4
+om_parent=$5
+om_template_id=340402
+
+if [ $# -lt 5 ]; then
+  echo 1>&2 "Usage: omify_waterSystemElement.sh entity_type entity_id $vahydro_model_pid system_name om_parent [om_template_id=$om_template_id] "
+  exit 2
+fi 
+
+if [ $# -gt 5 ]; then
+  om_template_id=$6
+fi 
+# create a remote om element below the indicated parent 
+# and then push the changes from the vahydro entity to the remote OM record
+echo "drush scr modules/om/src/om_setprop.php cmd dh_properties $pid om_element_connection om_element_connection NULL clone \"om_template_id=$om_template_id&remote_parentid=$om_parent\" "
+drush scr modules/om/src/om_setprop.php cmd dh_properties $pid om_element_connection om_element_connection NULL clone "om_template_id=$om_template_id&remote_parentid=$om_parent"
+drush scr modules/om/src/om_saveprop.php cmd $entity_type $entity_id "$system_name"
