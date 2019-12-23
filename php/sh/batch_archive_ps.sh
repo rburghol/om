e89b5c2f788759b6d8ba948464ea364b9819821c
Burgholzer <robert.burgholzer@deq.virginia.gov>
2019-12-23 12:25:02 -0500

    Archvie PS beta read

--- php/sh/batch_archive_ps.sh
+++ php/sh/batch_archive_ps.sh
@@ -4,7 +4,7 @@ archive_query=`cat ./create_ps_archive.sql`
 echo $archive_query | psql -h dbase2 model
 archive_export=`cat ./best_ps_archives.sql`
 echo $archive_export | psql -h dbase2 model > /tmp/ps_archives.txt 
-echo "drop table tmp_runoff_archives;" | psql -h dbase2 model > /tmp/ps_archives.txt 
+echo "drop table tmp_ps_archives;" | psql -h dbase2 model > /tmp/ps_archives.txt 
 
 n=`< /tmp/ps_archives.txt wc -l`
 nm="$((n - 2))"
