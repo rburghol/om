4619f3cbbc6bc73a3d1fd73488f93a6e3c7df6a6
Burgholzer <robert.burgholzer@deq.virginia.gov>
2019-12-23 12:17:31 -0500

    Archvie PS beta read

--- php/sh/best_ps_archives.sql
+++ php/sh/best_ps_archives.sql
@@ -3,13 +3,11 @@ select a.custom2 as riverseg,
   d.elementid as oldelement, 
   rf.src_file
 from scen_model_element as a 
--- grab the WS/PS container
 left outer join map_model_linkages as l1   
 on (
   a.elementid = l1.dest_id 
   and l1.linktype = 1
 ) 
--- Grab the PS sub-container
 left outer join scen_model_element as c  
 on (c.elementid = l1.src_id)  
 left outer join map_model_linkages as l2   
@@ -17,9 +15,12 @@ on (
   c.elementid = l2.dest_id 
   and l2.linktype = 1
 ) 
+left outer join scen_model_element as d  
+on (d.elementid = l2.src_id and d.custom1 = 'cova_ps_group')  
 left outer join tmp_ps_archives as rf
 on (rf.elementid = d.elementid)
-where c.custom1 = 'cova_ps_group' 
+where c.custom1 = 'cova_pswd' 
   and a.scenarioid = 37 
+  and d.elementid is not null 
   and src_file is not null
 ;
