ae98eafe12af71e696975aa613ad82d892c514e5
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2019-12-27 15:50:42 -0500

    debug cache

--- drupal/om/sh/default_consumption.sh
+++ drupal/om/sh/default_consumption.sh
@@ -2,5 +2,5 @@
 
 filename=$1
 for i in `cat $filename`; do
-  drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties 4988636 dh_properties $i consumption_monthly; 
+  drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties 4988636 dh_properties $i "consumption_monthly_cws_auto|consumption_monthly"; 
 done
