bff0ed85bfe002a49dfd0502b59d53f62e8a3d75
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2020-01-07 11:36:06 -0500

    Added summarize_element.sh

--- drupal/om/sh/summarize_element.sh
+++ drupal/om/sh/summarize_element.sh
@@ -10,13 +10,13 @@ runid=$2
 # @todo: Put in batch post-process and summarize into vahydro 
 # Also, replace the normal dH/VAHydro call to php 
 # with a call to a shell script that runs the model, then runs a R summary script 
-#
+
 # summarize all runoff containers 
-pid=`drush scr modules/om/src/om_get_model_info.php $elid`
+info=`drush scr modules/om/src/om_get_model_info.php $elid`
 cd /var/www/R
 # Rscript vahydro/R/post.runoff.R $pid $elid $runid
-while IFS= read -r line; do
-    #echo "Text read from file: $line"
-    IFS="$IFS|" read pid elid <<< "$line"
-    Rscript /opt/model/vahydro/R/post.runoff.R $pid $elid $runid $tyear 
-done < /tmp/runoff-models.txt 
+IFS="$IFS|" read pid elid object_class <<< "$info"
+sum_script="/opt/model/vahydro/R/summarize.${object_class}.R"
+if test -f "$sum_script"; then
+    Rscript $sum_script $pid $elid $runid 
+fi
