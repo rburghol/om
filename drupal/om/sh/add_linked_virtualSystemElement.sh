f26f09f41c526b95a40127d9ec7d365b72ab7ea9
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2020-01-09 16:30:27 -0500

    Common water element clone help

--- drupal/om/sh/add_linked_virtualSystemElement.sh
+++ drupal/om/sh/add_linked_virtualSystemElement.sh
@@ -16,7 +16,7 @@ if [ $# -gt 3 ]; then
 fi 
 
 # copy the element fac_current_mgy from it's feature wd_current_mgy 
-drush scr modules/om/src/om.model.wsp.props.php cmd $pid $entity_id om_class_Equation sw_frac riverseg_frac $entity_type 
+drush scr modules/om/src/om.model.wsp.props.php cmd $pid $entity_id om_class_Equation riverseg_frac sw_frac $entity_type 
 drush scr modules/om/src/om.model.wsp.props.php cmd $pid $entity_id om_class_Equation wsp2020_2020_mgy wsp2020_2020_mgy $entity_type 
 drush scr modules/om/src/om.model.wsp.props.php cmd $pid $entity_id om_class_Equation wsp2020_2040_mgy wsp2020_2040_mgy $entity_type
 # Copy monthly from template, NOT linked to facility
