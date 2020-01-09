ee0eb9b7b0fc4b20eda3cb6812a8d681059e3165
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2020-01-09 17:19:03 -0500

    Reduce redundant save

--- drupal/om/sh/omify_waterSystemElement.sh
+++ drupal/om/sh/omify_waterSystemElement.sh
@@ -22,7 +22,5 @@ echo "drush scr modules/om/src/om_setprop.php cmd dh_properties $vahydro_model_p
 
 drush scr modules/om/src/om_setprop.php cmd dh_properties $vahydro_model_pid om_element_connection om_element_connection 0 clone "om_template_id=$om_template_id&remote_parentid=$om_parent"
 
-drush scr modules/om/src/om_setprop.php cmd dh_properties $pid om_element_connection om_element_connection NULL clone "om_template_id=$om_template_id&remote_parentid=$om_parent"
-
 echo "drush scr modules/om/src/om_saveprop.php cmd $entity_type $entity_id \"$system_name\""
 drush scr modules/om/src/om_saveprop.php cmd $entity_type $entity_id "$system_name"
