a2650b752380afcd6997f746de1bf262d770d365
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2020-01-10 07:26:14 -0500

    Debug copy monthly pct

--- drupal/om/sh/add_linked_virtualSystemElement.sh
+++ drupal/om/sh/add_linked_virtualSystemElement.sh
@@ -4,7 +4,7 @@ pid=$1
 entity_type=$2
 entity_id=$3
 # Virtual County Facility Model Template
-template=340402
+template=5234733
 
 if [ $# -lt 3 ]; then
   echo 1>&2 "Usage: add_linked_virtualSystemElement.sh pid entity_type entity_id [template$template]"
@@ -21,5 +21,6 @@ drush scr modules/om/src/om.model.wsp.props.php cmd $pid $entity_id om_class_Equ
 drush scr modules/om/src/om.model.wsp.props.php cmd $pid $entity_id om_class_Equation wsp2020_2020_mgy wsp2020_2020_mgy $entity_type 
 drush scr modules/om/src/om.model.wsp.props.php cmd $pid $entity_id om_class_Equation wsp2020_2040_mgy wsp2020_2040_mgy $entity_type
 # Copy monthly from template, NOT linked to facility
+echo "drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid historic_monthly_pct "
 drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid historic_monthly_pct 
 # Calculate the riverseg frac ... this could be tough = sum(fac:mp) in riverseg, divided by fac_current_mgy 
