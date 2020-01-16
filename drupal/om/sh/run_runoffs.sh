1a34e0cdfb1f01534b952e4a2a9a4d74620afd5c
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2020-01-16 09:04:37 -0500

    Took out post process from run_runoffs scripts because this is no automatic in run_model.php

--- drupal/om/sh/run_runoffs.sh
+++ drupal/om/sh/run_runoffs.sh
@@ -43,35 +43,3 @@ tail -n $nm /tmp/head.txt > /tmp/runoffs.txt
 
 cd /var/www/html/om/
 /usr/bin/php run_shakeTree.php 6 '/tmp/runoffs.txt' $runid $startdate $enddate $cachedate $force 37 -1 $run_mode normal flow_mode=$flow_mode 
-
-# @todo: Put in batch post-process and summarize into vahydro 
-# Also, replace the normal dH/VAHydro call to php 
-# with a call to a shell script that runs the model, then runs a R summary script 
-#
-# run all runoff containers 
-q="select a.pid, b.propvalue "
-q="$q from dh_properties as a "
-q="$q left outer join dh_properties as b "
-q="$q on (a.pid = b.featureid and b.propname = 'om_element_connection' and b.entity_type = 'dh_properties') "
-q="$q left outer join dh_properties as m "
-q="$q on (a.featureid = m.pid) "
-q="$q left outer join dh_feature as c "
-q="$q on (m.featureid = c.hydroid ) "
-q="$q where a.propname = '1. Local Runoff Inflows'"
-q="$q and substring(replace(c.hydrocode,'vahydrosw_wshed_', ''),1,1) in ('P','J','Y','R')"
-echo $q | psql -h dbase2 drupal.dh03 > /tmp/runoff-models.txt 
-
-n=`< /tmp/runoff-models.txt wc -l`
-nm="$((n - 2))"
-head -n $nm /tmp/runoff-models.txt > /tmp/head.txt 
-n=`< /tmp/head.txt wc -l`
-nm="$((n - 2))"
-tail -n $nm /tmp/head.txt > /tmp/runoff-models.txt 
-
-cd /var/www/R
-# Rscript vahydro/R/post.runoff.R $pid $elid $runid
-while IFS= read -r line; do
-    #echo "Text read from file: $line"
-    IFS="$IFS|" read pid elid <<< "$line"
-    Rscript /opt/model/vahydro/R/post.runoff.R $pid $elid $runid
-done < /tmp/runoff-models.txt 
