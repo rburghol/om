bb1e811549a70299b1a78e8faa786ba709b03ce4
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2020-02-10 16:34:45 -0500

    Added fn_rename_group_subcomp.php

--- php/src/fn_rename_group_subcomp.php
+++ php/src/fn_rename_group_subcomp.php
@@ -2,7 +2,6 @@
 
 $noajax = 1;
 $projectid = 3;
-$target_scenarioid = 37;
 $cbp_scenario = 4;
 $userid = 1;
 $noajax = 1;
@@ -11,10 +10,10 @@ error_reporting(E_ERROR);
 //include_once("./lib_batchmodel.php");
 
 if (count($argv) < 4) {
-   print("Usage: php fn_rename_group_subcomp.php scenarioid [subcomp1|newname1],subcomp2...] [elementid] [elemname] [custom1] [custom2] \n");
-   print("Use '-1' as value for scenarioid to update all scenarios (use with caution) \n");
-   print("Example (rename \"Listen on Child\" to \"Listen to Children\" on 213933 \n");
-   print("php fn_rename_group_subcomp.php 37 \"Listen on Child|Listen to Children\" 213933 \n");
+   error_log("Usage: php fn_rename_group_subcomp.php scenarioid [subcomp1|newname1],subcomp2...] [elementid] [elemname] [custom1] [custom2]");
+   error_log("Use '-1' as value for scenarioid to update all scenarios (use with caution)");
+   error_log("Example (rename \"Listen on Child\" to \"Listen to Children\" on 213933");
+   error_log("php fn_rename_group_subcomp.php 37 \"Listen on Child|Listen to Children\" 213933");
    die;
 }
 
@@ -61,15 +60,14 @@ if ($custom1 <> '') {
 if ($custom2 <> '') {
    $listobject->querystring .= " AND custom2 = '$custom2' ";
 }
-print("$listobject->querystring ; <br>");
+error_log("$listobject->querystring ; <br>");
 $listobject->performQuery();
-//$listobject->showList();
 
 $recs = $listobject->queryrecords;
 //error_reporting(E_ALL);
 foreach ($recs as $thisrec) {
   $elid = $thisrec['elementid'];
-  print("Editing $subcomp_name on $elemname ($elid) \n");
+  error_log("Editing $subcomp_name on $elemname ($elid) \n");
   $loadres = unSerializeSingleModelObject($elid);
   $thisobject = $loadres['object'];
   foreach ($subcomps as $thiscomp) {
@@ -77,12 +75,13 @@ foreach ($recs as $thisrec) {
     if ( (count($scs) == 1) or ($scs[0] == $scs[1]) ) {
       continue;
     }
-    print("Trying to rename Sub-comp $scs[0] to $scs[1] <br>\n" . print_r($scs,1) . "\n");
-    $thisobject->processors[$scs[1]] = $thisobject->processors[$scs[0]];
-    unset($thisobject->processors[$scs[0]]);
-    saveObjectSubComponents($listobject, $thisobject, $elid, 1);
-    print("$cr<br>\n");
-    print("$msg<br>\n");
+    error_log("Trying to rename Sub-comp $scs[0] to $scs[1] on element $elid ");
+    if (isset($thisobject->processors[$scs[0]])) {
+      $thisobject->processors[$scs[1]] = $thisobject->processors[$scs[0]];
+      unset($thisobject->processors[$scs[0]]);
+      saveObjectSubComponents($listobject, $thisobject, $elid, 1);
+    } else {
+      error_log("Property $scs[0] does not exist ... skipping.");
     $i++;
   }
 }
