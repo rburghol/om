88260b5a9079d1adddcc732a6a4290fae2b96a7e
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2020-02-11 10:17:09 -0500

    Edited broadcase batch edit

--- php/src/fn_batchedit_broadcast_matrix.php
+++ php/src/fn_batchedit_broadcast_matrix.php
@@ -12,8 +12,8 @@ $projectid = 3;
 error_reporting(E_ERROR);
 
 if ( count($argv) < 3 ) {
-   print("Usage: edit_submatrix.php scenarioid matrix_name \"prop=value\" [elementid] [elemname] [custom1] [custom2] [function (append,overwrite,delete)]\n");
-   print("Use '-1' as value for scenarioid to update all scenarios (use with caution) \n");
+   error_log("Usage: edit_submatrix.php scenarioid matrix_name \"prop=value\" [elementid] [elemname] [custom1] [custom2] [function (append,overwrite,delete)]\n");
+   error_log("Use '-1' as value for scenarioid to update all scenarios (use with caution) \n");
    die;
 }
 
@@ -62,37 +62,37 @@ if ($custom1 <> '') {
 if ($custom2 <> '') {
    $listobject->querystring .= " AND custom2 = '$custom2' ";
 }
-print("Looking for match <br>\n");
-print("$listobject->querystring ; <br>\n");
+error_log("Looking for match <br>\n");
+error_log("$listobject->querystring ; <br>\n");
 $listobject->performQuery();
 $recs = $listobject->queryrecords;
 
 foreach ($recs as $thisrec) {
    $elid = $thisrec['elementid'];
    $elemname = $thisrec['elemname'];
-   print("Editing $subcomp_name on $elemname ($elid) \n");
+   error_log("Editing $subcomp_name on $elemname ($elid) \n");
    $loadres = unSerializeSingleModelObject($elid);
    $thisobject = $loadres['object'];
 
    if (is_object($thisobject)) {
       if (isset($thisobject->processors[$subcomp_name])) {
-         print("Editing Matrix $subcomp_name\n ");
-         $thisobject->processors[$subcomp_name]->formatMatrix();
-         $orig = $thisobject->processors[$subcomp_name]->matrix_formatted;
-         print("Original Matrix: " . print_r($orig,1) . "\n");
+         error_log("Editing Matrix $subcomp_name\n ");
+         $thisobject->processors[$subcomp_name]->matrix->formatMatrix();
+         $orig = $thisobject->processors[$subcomp_name]->matrix->->matrix_formatted;
+         error_log("Original Matrix: " . error_log_r($orig,1) . "\n");
          $orig[$prop] = $value;
          ksort($orig);
-         print("Modified Matrix: " . print_r($orig,1) . "\n");
-         $thisobject->processors[$subcomp_name]->oneDimArrayToMatrix($orig);
-         $thisobject->processors[$subcomp_name]->formatMatrix();
-         $mod = $thisobject->processors[$subcomp_name]->matrix_formatted;
-         print("Final Matrix: " . print_r($mod,1) . "\n");
+         error_log("Modified Matrix: " . error_log_r($orig,1) . "\n");
+         $thisobject->processors[$subcomp_name]->matrix->->oneDimArrayToMatrix($orig);
+         $thisobject->processors[$subcomp_name]->matrix->->formatMatrix();
+         $mod = $thisobject->processors[$subcomp_name]->matrix->->matrix_formatted;
+         error_log("Final Matrix: " . error_log_r($mod,1) . "\n");
          //$thisobject->processors[$subcomp_name]->$prop = $value;
          saveObjectSubComponents($listobject, $thisobject, $elid );
       }
    }
 }
    
-print("Finished.\n");
+error_log("Finished.\n");
 
 ?>
