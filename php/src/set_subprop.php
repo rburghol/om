deaca000dc253f155e4e43b970ab955423d05d51
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2020-02-12 11:59:44 -0500

    Handle json2d

--- php/src/set_subprop.php
+++ php/src/set_subprop.php
@@ -76,6 +76,7 @@ if (is_object($thisobject)) {
     //    $thisobject->processors['impoundment']->setProp('storage_stage_area', 'JSON storage table');
     //   * These should also omit the object_class since they should fail if they do not exist, rather than adding
     //error_log("Calling thisobject->processors[$comp_name]->setProp($subprop_name, $subprop_value, $setprop_mode); on object of class " . get_class($thisobject->processors[$comp_name]));
+    error_log("Updateing $subprop_name with mode $setprop_mode");
     $thisobject->processors[$comp_name]->setProp($subprop_name, $subprop_value, $setprop_mode);
     $thisobject->processors[$comp_name]->objectclass = $comp_class;
   }
