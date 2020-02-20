21eb8bc13132b5231190cc9667b32086bfb558b3
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2020-02-20 11:18:24 -0500

    Added new consumptive use adding script which guarantees monthly percent

--- drupal/om/sh/batch_add_cws_ps.sh
+++ drupal/om/sh/batch_add_cws_ps.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 filename=$1
 while IFS= read -r line; do
   #echo "Text read from file: $line"
   IFS="$IFS|" read pid entity_type entity_id <<< "$line"
-  ./modules/om/sh/add_cws_ps.sh $i
+  ./modules/om/sh/add_cws_ps.sh $pid $entity_type $entity_id
 done < $filename 
