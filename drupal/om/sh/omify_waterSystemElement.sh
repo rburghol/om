1fc1a989a6aedacf926f60837ba10451573e23c6
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2020-02-22 06:55:31 -0500

    Expanded list of matrices that get saved when omifying an existing model

--- drupal/om/sh/omify_waterSystemElement.sh
+++ drupal/om/sh/omify_waterSystemElement.sh
@@ -22,6 +22,14 @@ echo "drush scr modules/om/src/om_setprop.php cmd dh_properties $vahydro_model_p
 
 drush scr modules/om/src/om_setprop.php cmd dh_properties $vahydro_model_pid om_element_connection om_element_connection NULL clone "om_template_id=$om_template_id&remote_parentid=$om_parent"
 
-
+# Save the vahydro copy of the model, which will cause it to push all default Properties to OM
 echo "drush scr modules/om/src/om_saveprop.php cmd $entity_type $entity_id \"$system_name\""
 drush scr modules/om/src/om_saveprop.php cmd $entity_type $entity_id "$system_name"
+# Save a few extras that won't be pushed by the base save, like matrices
+# If the VAHydro model has the property in question, then this will save it, otherwise, it will just move on
+drush scr modules/om/src/om_saveprop.php cmd dh_properties $vahydro_model_pid consumption_monthly
+drush scr modules/om/src/om_saveprop.php cmd dh_properties $vahydro_model_pid ps_enabled
+drush scr modules/om/src/om_saveprop.php cmd dh_properties $vahydro_model_pid fac_demand_mgy
+drush scr modules/om/src/om_saveprop.php cmd dh_properties $vahydro_model_pid wd_mgd
+drush scr modules/om/src/om_saveprop.php cmd dh_properties $vahydro_model_pid demand_mgd
+drush scr modules/om/src/om_saveprop.php cmd dh_properties $vahydro_model_pid discharge_mgd
