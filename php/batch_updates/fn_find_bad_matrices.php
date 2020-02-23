976ee24479ff38a172abac2c77b7c38e7b83f7f7
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2020-02-23 09:17:28 -0500

    Added fn_find_bad_matrices

---
+++ php/batch_updates/fn_find_bad_matrices.php
@@ -0,0 +1,55 @@
+<?php
+
+$noajax = 1;
+$projectid = 3;
+$userid = 1;
+$scenarioid = 37;
+$wd_template_id = 284895;
+# New Generic Surface Water User: 340402
+
+include_once('xajax_modeling.element.php');
+//error_reporting(E_ALL);
+##include_once("./lib_batchmodel.php");
+
+if (count($argv) < 2) {
+   print("Usage: fn_set_vahydro1_hydrocode.php riverseg  \n");
+   die;
+}
+error_log(print_r($argv,1));
+$riverseg = $argv[1];
+
+if ($riverseg == 'all') {
+  $listobject->querystring = "select elementid, custom2 from scen_model_element where custom1 = 'cova_ws_container' and scenarioid = $scenarioid ";
+  $listobject->performQuery();
+  $rsegs = $listobject->queryrecords;
+} else {
+  $elid = getCOVACBPContainer($listobject, $scenarioid, $riverseg);
+  $rsegs = array(
+    0 => array( 'elementid' => $elid, 'custom2' => $riverseg )
+  );
+}
+
+foreach ($rsegs as $seg) {
+  $elid = $seg['elementid'];
+  $riverseg = $seg['custom2'];
+  $wds = getCOVAWithdrawals($listobject, $elid, array(), 1);
+  $alwds = getAtLargeWithdrawals($listobject, $elid, array(), 1);
+  $wds = array_merge($wds, $alwds);
+  foreach ($wds as $thiswd) {
+    $wd_elid = $thiswd['elementid'];
+    $loadres = unSerializeSingleModelObject($wd_elid);
+    $wdobject = $loadres['object'];
+    // check first for new method, with props.
+    // this is the VWUDS/VADEQ UserID value
+    $hydrocode = $wdobject->getProp('id1', 'value');
+    $wdtype = $wdobject->getProp('wdtype', 'value');
+    $q = "update scen_model_element set hydrocode = '$hydrocode', wdtype = '$wdtype', riverseg = '$riverseg' where elementid = $wd_elid";
+    $listobject->querystring = $q;
+    error_log($q);
+    $listobject->performQuery();
+  }
+}
+
+
+
+?>
---
+++ php/src/fn_find_bad_matrices.php
@@ -0,0 +1,50 @@
+<?php
+
+$noajax = 1;
+$projectid = 3;
+$userid = 1;
+$scenarioid = 37;
+$wd_template_id = 284895;
+# New Generic Surface Water User: 340402
+
+include_once('xajax_modeling.element.php');
+//error_reporting(E_ALL);
+##include_once("./lib_batchmodel.php");
+
+if (count($argv) < 3) {
+   print("Usage: fn_set_vahydro1_hydrocode.php scenario custom1 [elementid] \n");
+   die;
+}
+error_log(print_r($argv,1));
+$scenario = $argv[1];
+$custom1 = $argv[2];
+$elementid = isset($argv[3]) ? $argv[3] : -1;
+
+$listobject->querystring = "select elementid, custom2 from scen_model_element where custom1 = '$custom1' and scenarioid = $scenarioid ";
+if ($elementid > 0) {
+  $listobject->querystring .= " and elementid = $elementid ";
+}
+$listobject->performQuery();
+$rsegs = $listobject->queryrecords;
+
+foreach ($elements as $element) {
+  $elid = $element['elementid'];
+  $riverseg = $element['custom2'];
+  $loadres = unSerializeSingleModelObject($elid);
+  $object = $loadres['object'];
+  
+  foreach ($object->processors as $thisproc) {
+    // check first for new method, with props.
+    // this is the VWUDS/VADEQ UserID value
+    if (get_class($thisproc) == 'dataMatrix') {
+      $count = count($thisproc->matrix);
+      if (!($count > 0)) {
+        error_log("$thisproc->name on Element $object->name ($elid) is empty");
+      }
+    }
+  }
+}
+
+
+
+?>
