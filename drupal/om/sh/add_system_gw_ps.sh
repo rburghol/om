4b0fd771b3d97f220350346e2ca7a5401048a44d
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2020-03-15 15:54:37 -0400

    Added add_system_gw_ps.sh

--- drupal/om/sh/add_system_gw_ps.sh
+++ drupal/om/sh/add_system_gw_ps.sh
@@ -13,5 +13,9 @@ template=4988636
 
 # add facility gw_frac variable as an equation on model
 drush scr modules/om/src/om.model.wsp.props.php cmd $pid $entity_id om_class_Equation gw_frac gw_frac $entity_type 
+drush scr modules/om/src/om.model.wsp.props.php cmd $pid $entity_id om_class_Equation sw_frac sw_frac $entity_type 
 # make sure it is using the new discharge_mgd variable 
 drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid discharge_mgd
+drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid gw_sw_factor
+drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid gw_demand_mgd
+drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid gw_discharge_mgd
