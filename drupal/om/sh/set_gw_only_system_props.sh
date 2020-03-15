ec09508b1290e354b55155f0b79b50f06670515d
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2020-03-15 17:35:27 -0400

    Added set_ge_only_system_props.sh

---
+++ drupal/om/sh/set_gw_only_system_props.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ $# -lt 1 ]; then
+  echo 1>&2 "Usage: add_system_gw_ps.sh vahydro_model_pid"
+  exit 2
+
+# Water Supply Model Element Template 
+template=5564183
+
+
+# make sure it is using the new discharge_mgd variable 
+drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid riverseg_frac
+drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid wd_mgd
+drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid base_demand_mgd
+drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid unmet_demand_mgd
+drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid gw_demand_mgd
+
