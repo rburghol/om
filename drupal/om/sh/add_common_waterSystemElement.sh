c11c29b376f09217636a09e53324e29b2f0f3705
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2020-03-16 17:25:26 -0400

    Added gw_frac to add_linked_waterSystemElement.sh

--- drupal/om/sh/add_common_waterSystemElement.sh
+++ drupal/om/sh/add_common_waterSystemElement.sh
@@ -17,3 +17,6 @@ drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_prop
 drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid fac_demand_mgy; 
 drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid wd_mgd;
 drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid discharge_mgd; 
+drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid gw_sw_factor
+drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid gw_demand_mgd
+drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid discharge_from_gw_mgd
