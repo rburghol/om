ac094e42ca9467d684884fa31661dea15aa0d7b5
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2020-03-16 16:32:10 -0400

    Added gw_frac to add_linked_waterSystemElement.sh

--- drupal/om/sh/add_linked_waterSystemElement.sh
+++ drupal/om/sh/add_linked_waterSystemElement.sh
@@ -17,6 +17,7 @@ fi
 
 # copy the element fac_current_mgy from it's feature wd_current_mgy 
 drush scr modules/om/src/om.model.wsp.props.php cmd $pid $entity_id om_class_Equation fac_current_mgy wd_current_mgy $entity_type 
+drush scr modules/om/src/om.model.wsp.props.php cmd $pid $entity_id om_class_Equation gw_frac gw_frac $entity_type 
 drush scr modules/om/src/om.model.wsp.props.php cmd $pid $entity_id om_class_Equation wsp2020_2020_mgy wsp2020_2020_mgy $entity_type 
 drush scr modules/om/src/om.model.wsp.props.php cmd $pid $entity_id om_class_Equation wsp2020_2040_mgy wsp2020_2040_mgy $entity_type 
 monpid=`drush scr modules/om/src/om_getpid.php $entity_type $entity_id "wd_current_mon_factors"`
