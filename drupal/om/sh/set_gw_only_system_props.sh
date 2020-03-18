dbabba7a4ea5e385a85907046e4024cd35fd346e
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2020-03-18 13:03:58 -0400

    Copy current_mgy from facility for gw only models.

--- drupal/om/sh/set_gw_only_system_props.sh
+++ drupal/om/sh/set_gw_only_system_props.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
 if [ $# -lt 1 ]; then
-  echo 1>&2 "Usage: set_gw_only_system_props.sh vahydro_model_pid"
+  echo 1>&2 "Usage: set_gw_only_system_props.sh vahydro_model_pid (entity_type=model parent, default=auto) (entity_id=model parent)"
   exit 2
 fi
 pid=$1
-
+entity_type='dh_feature'
+if [ $# -gt 1 ]; then
+  entity_type=$2
+fi 
+entity_id="auto"
+if [ $# -gt 2 ]; then
+  entity_id=$3
+fi 
 # Water Supply Model Element Template 
 template=5564183
 
@@ -17,4 +24,5 @@ drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_prop
 drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid unmet_demand_mgd
 drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid gw_demand_mgd
 drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid discharge_mgd
+drush scr modules/om/src/om.model.wsp.props.php cmd $pid $entity_id om_class_Equation current_mgy wd_current_mgy $entity_type 
 
