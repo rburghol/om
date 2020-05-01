8e4c4fcc39743159c04fddc569e5a4fe794ccc6d
Burgholzer <robert.burgholzer@deq.virginia.gov>
2020-05-01 11:44:38 -0400

    added returnCommands script

--- php/lib/misc_functions.php
+++ php/lib/misc_functions.php
@@ -566,14 +566,15 @@ function returnPids($command) {
 
 function returnCommands($command, $like='') {
   exec("ps -C $command -o command=",$cmds);
+  $ret = array();
   foreach ($cmds as $key=>$value) {
     if (strlen($like) > 0) {
-      error_log("Sea3rching for $like in $value -> " . strpos($value, $like));
+      //error_log("Sea3rching for $like in $value -> " . strpos($value, $like));
       if (!(strpos($value, $like) === FALSE)) {
-        $cmds[$key]=trim($value);
+        $ret[$key]=trim($value);
       }
     } else {
-      $cmds[$key]=trim($value);
+      $ret[$key]=trim($value);
     }
   }
   return $cmds;
