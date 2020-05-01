8dc27901aa88ea58ebbabe03a10faa356978017e
Burgholzer <robert.burgholzer@deq.virginia.gov>
2020-05-01 14:24:37 -0400

    info

--- php/src/fn_checkTreeRunDate.php
+++ php/src/fn_checkTreeRunDate.php
@@ -13,7 +13,7 @@ include_once("./lib_batchmodel.php");
 if (isset($argv[1])) {
    $elementid = $argv[1];
 } else { 
-   print("Usage: php fn_checkTreeRunDate.php elementid runid [startdate] [enddate] [cachedate]\n");
+   print("Usage: php fn_checkTreeRunDate.php elementid runid [startdate] [enddate] [cachedate] [debug=0]\n");
    die;
 }
 if (isset($argv[2])) {
@@ -23,11 +23,12 @@ if (isset($argv[2])) {
    die;
 }
 $startdate = isset($argv[3]) ? $argv[3] : '1984-01-01';
-$enddate = isset($argv[3]) ? $argv[4] : '2005-12-31';
-$cache_date = isset($argv[3]) ? $argv[5] : date('Y-m-d');
+$enddate = isset($argv[4]) ? $argv[4] : '2005-12-31';
+$cache_date = isset($argv[5]) ? $argv[5] : date('Y-m-d');
+$debug = isset($argv[6]) ? $argv[6] : $0;
 error_reporting(E_ALL);
 print("Clearing elementid $elementid, Run - $runid \n");
-$status = checkTreeRunDate($listobject, $elementid, $runid, $startdate, $enddate, $cache_date);
+$status = checkTreeRunDate($listobject, $elementid, $runid, $startdate, $enddate, $cache_date, $debug);
 print("Status: $status.\n");
 
 $is_run = verifyRunStatus($listobject, $elementid, $runid, $serverip);
