76f738f0d2664e0eb426e2024ace430b8a03c4a0
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2020-08-28 14:32:31 -0400

    Added new flowby vars to sh config scripts

--- drupal/om/sh/add_flowby_drought_waterSystemElement.sh
+++ drupal/om/sh/add_flowby_drought_waterSystemElement.sh
@@ -19,8 +19,20 @@ drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_prop
 drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid drought_pct; 
 drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid flowby; 
 drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid flowby_current; 
-drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid flowby_future; 
+drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid flowby_proposed; 
 drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid flowby_historic; 
 drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid available_mgd; 
+  drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid vwp_base_mgd
+  drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid vwp_demand_mgd
+  drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid vwp_prop_max_mgd
+  drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid vwp_prop_max_mgy
+  drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid vwp_prop_base_mgd
+  drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid vwp_prop_demand_mgd
+  drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid vwp_max_mgy
+  drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid vwp_max_mgd
+  drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid vwp_base_mgd
+  drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid vwp_demand_mgd
+
+
 # Deprecated - was old part of vwp templates.  Try to remove.
 #drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid pump_mgd; 
