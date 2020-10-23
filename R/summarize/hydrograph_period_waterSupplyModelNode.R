9de6229772c665e49efb569315c03045e9a794b4
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2020-10-23 14:31:38 -0400

    group to daily before runnign iha

--- R/summarize/hydrograph_period_waterSupplyModelNode.R
+++ R/summarize/hydrograph_period_waterSupplyModelNode.R
@@ -1,18 +1,23 @@
-elid = 236975 # cootes: , mount j: 229937, strasburg: 230667
+basepath='/var/www/R';
+source(paste(basepath,'config.R',sep='/'))
+
+elid = 230533 # cootes: , mount j: 229937, strasburg: 230667
 # sf shen at front royal: 229799
+# sf shen lynwood: 234585 PS4_6360_5840
 # shenandoah river at Potomac Confluence, Millville WV: 230533
 # New river above Claytor 277538
 # Point of Rocks 233997 
 # James at Richmond
 # Opequpon creek 236975 
-gage_number = '01615000' 
+gage_number = '01636500' 
 # cootes: 01632000, mount j: 01633000, strasburg: 01634000
 # Shen at Millville WV: 01636500
+# sf lynwood: 01628500
 # New River above claytor: 03168000
 # Potomac River @ PoR 01638500
 # Opequon creek: 01615000
-startdate <- "2002-10-01"
-enddate <- "2002-12-31"
+startdate <- "2002-05-01"
+enddate <- "2002-11-30"
 
 # Get and format gage data
 gage_data <- gage_import_data_cfs(gage_number, startdate, enddate)
@@ -35,8 +40,8 @@ plot(
   main="Daily Model Timestep"
 )
 lines(gage_data$flow, col='blue')
-
-
+# runid: 
+  # 1131 = hourly, 1998-2002
 runid = 1131
 finfo = fn_get_runfile_info(elid, runid, 37, site= omsite)
 dat <- fn_get_runfile(elid, runid, site= omsite,  cached = FALSE)
@@ -80,6 +85,11 @@ plot(
 lines(gage_data$flow, col='blue')
 
 
+# runid: 
+# 1131 = hourly, 1998-2002
+# 1151 - 6-hour, 1998-2002
+# 1152 - 4-hour, 1998-2002
+# 1153 - 3-hour, 1998-2002
 runid = 1151
 finfo = fn_get_runfile_info(elid, runid, 37, site= omsite)
 dat <- fn_get_runfile(elid, runid, site= omsite,  cached = FALSE)
@@ -117,6 +127,60 @@ plot(
   ylab="Flow/WD/PS (cfs)",
   xlab=paste("Model vs USGS",startdate,"to",enddate),
   main="6-Hour Model Timestep Average to Daily",
+  legend=c('Model', 'USGS')
+)
+lines(gage_data$flow, col='blue')
+
+# 1153 - 3-hour, 1998-2002
+runid = 1153
+finfo = fn_get_runfile_info(elid, runid, 37, site= omsite)
+dat <- fn_get_runfile(elid, runid, site= omsite,  cached = FALSE)
+mode(dat) <- 'numeric'
+# analyze
+datdaily <- aggregate(
+  dat,
+  as.POSIXct(
+    format(
+      time(dat), 
+      format='%Y/%m/%d'),
+    tz='UTC'
+  ),
+  'mean'
+)
+group2(datdaily$Qout)
+#limit to period
+datpd <- window(dat, start = startdate, end = enddate)
+
+
+# Plot
+ymx <- max(datpd$Qout,gage_data$flow)
+plot(
+  datpd$Qout, ylim = c(0,ymx),
+  ylab="Flow/WD/PS (cfs)",
+  xlab=paste("Model vs USGS",startdate,"to",enddate),
+  main="3-Hour Model Timestep"
+)
+lines(gage_data$flow, col='blue')
+
+# Hourly to Daily flow timeseries
+datpd = aggregate(
+  datpd,
+  as.POSIXct(
+    format(
+      time(datpd), 
+      format='%Y/%m/%d'),
+    tz='UTC'
+  ),
+  'mean'
+)
+
+# Plot
+ymx <- max(datpd$Qout,gage_data$flow)
+plot(
+  datpd$Qout, ylim = c(0,ymx),
+  ylab="Flow/WD/PS (cfs)",
+  xlab=paste("Model vs USGS",startdate,"to",enddate),
+  main="3-Hour Model Timestep Average to Daily",
   legend=c('Model', 'USGS')
 )
 lines(gage_data$flow, col='blue')
