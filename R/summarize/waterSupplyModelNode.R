f895024171d7121ed2738124c1f798e386857bfe
Joey Kleiner <jkleiner@vt.edu>
2020-12-15 17:59:35 -0500

    Update waterSupplyModelNode.R

--- R/summarize/waterSupplyModelNode.R
+++ R/summarize/waterSupplyModelNode.R
@@ -4,7 +4,7 @@ site <- "http://deq2.bse.vt.edu/d.dh"    #Specify the site of interest, either d
 # Load Libraries
 basepath='/var/www/R';
 source(paste(basepath,'config.R',sep='/'))
-source(paste(elfgen_location,'R','elfgen.R',sep='/'))
+source(paste(elfgen_location,'R','elfdata-vahydro.R',sep='/'))
 print(elfgen_location)
 source(paste(om_location,'R/summarize','rseg_elfgen.R',sep='/'))
 library(stringr)
