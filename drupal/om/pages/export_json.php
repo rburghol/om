7b26d2215713b49240a80acd5ebddff855d3c052
Burgholzer <robert.burgholzer@deq.virginia.gov>
2021-06-02 08:03:41 -0400

    export script can run in node and return json to R seamlessly

--- drupal/om/pages/export_json.php
+++ drupal/om/pages/export_json.php
@@ -6,8 +6,9 @@ $pid = NULL;
 
 if (isset($a[2])) {
   $pid = $a[2];
+  $mode = $a[3];
 } else {
-  echo "Usage: /[pid]<br>";
+  echo "Usage: /[pid]/mode(json,vardef,debug)<br>";
 }
 if ($pid <> NULL) {
   $prop = entity_load_single('dh_properties', $pid);
@@ -16,14 +17,27 @@ if ($pid <> NULL) {
   $vars = array();
   $plugin->exportVarDefs($prop, $vars);
 
-  dpm($exp,'Export');
-  dpm($vars,'Vars');
+  //dpm($exp,'Export');
+  //dpm($vars,'Vars');
 
-
+  // tis may no lionger be used, as drupal_export_json handles it?  But we *could* use this if it is better.
   $exp_json = json_encode($exp, JSON_PRETTY_PRINT);
   $vars_json = json_encode($vars, JSON_PRETTY_PRINT);
 
-  echo "<pre>$exp_json</pre>";
-  echo "<pre>$vars_json</pre>";
+  switch ($mode) {
+    case 'vardef':
+      drupal_json_output($vars);
+      drupal_exit();
+    break;
+    case 'debug':
+      echo "<pre>$vars_json</pre>";
+      echo "<pre>$exp_json</pre>";
+    break;
+    case 'json':
+    default:
+      drupal_json_output($exp);
+      drupal_exit();
+    break;
+  }
 }
 ?>
