7e3657971f57e0ccc8713dc8d47e9f3672a54a1c
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2021-06-10 15:24:38 -0400

    added script to paramterize a system with a small off stream impoundment (maybe pump store)

---
+++ drupal/om/sh/set_local_impoundment_system_props.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+if [ $# -lt 1 ]; then
+  echo 1>&2 "This is used to model a water suply facility with off-line impoundment"
+  echo 1>&2 "Usage: set_local_impoundment_system_props.sh vahydro_model_pid (entity_type=model parent, default=auto) (entity_id=model parent)"
+  exit 2
+fi
+pid=$1
+entity_type='dh_feature'
+if [ $# -gt 1 ]; then
+  entity_type=$2
+fi 
+entity_id="auto"
+if [ $# -gt 2 ]; then
+  entity_id=$3
+fi 
+# Water Supply Model Element Template 
+template=6528239
+
+
+# make sure it is using the new discharge_mgd variable 
+drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid available_mgd
+drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid local_impoundment
+drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid refill_available_mgd
+drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid refill_plus_demand
+drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid wd_local_mgd
+drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid wd_mgd
+drush scr modules/om/src/om_copy_subcomp.php cmd dh_properties $template dh_properties $pid zero
+
