b9000d36b7fbfd4b9b6acea415990c5b86465f53
Burgholzer <robert.burgholzer@deq.virginia.gov>
2021-06-22 16:35:30 -0400

    use pipe

--- drupal/om/sh/init_runids.sh
+++ drupal/om/sh/init_runids.sh
@@ -2,9 +2,9 @@
 
 varid=`drush scr modules/om/src/om_getvardef.php om_scenario`
 variants_fid=`drush scr modules/om/src/om_getpid.php dh_variabledefinition $varid variants`
-while IFS=$'|' read -r line; do
+while read -r line; do 
   #echo "Text read from file: $line"
-  read run_id run_name run_abbrev <<< "$line"
+  IFS="|" read run_id run_name run_abbrev <<< "$line"
   propcode="runid_$run_id"
   echo "drush scr modules/om/src/om_setprop.php cmd dh_properties $variants_fid om_class_AlphanumericConstant '$run_name' $run_id $propcode"
   scen_pid=`drush scr modules/om/src/om_setprop.php cmd dh_properties $variants_fid om_scenario "$run_name" $run_id $propcode`
