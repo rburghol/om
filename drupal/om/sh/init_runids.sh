0b8014f8790dde0b9d6544cc9d547c50569cc464
Burgholzer <robert.burgholzer@deq.virginia.gov>
2021-06-22 15:19:01 -0400

    removed single quotes

--- drupal/om/sh/init_runids.sh
+++ drupal/om/sh/init_runids.sh
@@ -6,6 +6,6 @@ while IFS= read -r line; do
   #echo "Text read from file: $line"
   IFS="$IFS|" read run_id run_name <<< "$line"
   propcode="runid_$run_id"
-  echo "drush scr modules/om/src/om_setprop.php cmd dh_properties $variants_fid $varkey '$run_name' $run_id $propcode"
-#  drush scr modules/om/src/om_setprop.php cmd dh_properties $variants_fid $varkey '$run_name' $run_id $propcode
+  echo "drush scr modules/om/src/om_setprop.php cmd dh_properties $variants_fid om_class_AlphanumericConstant '$run_name' $run_id $propcode"
+  drush scr modules/om/src/om_setprop.php cmd dh_properties $variants_fid om_class_AlphanumericConstant '$run_name' $run_id $propcode
 done < /opt/model/om/drupal/om/data/run_ids.txt
