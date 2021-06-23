6bba8215d1089323507c2495b7691b4abb15bea3
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2021-06-23 12:40:37 -0400

    save all

--- drupal/om/src/push_remote.php
+++ drupal/om/src/push_remote.php
@@ -8,6 +8,10 @@ while ($arg = drush_shift()) {
 }
 $pid = $args[0];
 
-echo "pid:" . $pid;
+error_log( "pid:" . $pid);
+$obj = entity_load_single('dh_properties', $pid);
+$plugin = $plugin = dh_variables_getPlugins($obj);
+error_log("Trying to pushAllToRemote on plugin of type " . get_class($plugin));
+$plugin->pushAllToRemote($obj);
 
 ?>
