445b5ace59f43cc42e52816ea2bcff5254b10057
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2021-06-23 12:36:00 -0400

    save all

---
+++ drupal/om/src/push_remote.php
@@ -0,0 +1,13 @@
+#!/user/bin/env drush
+<?php
+module_load_include('inc', 'om', 'src/om_translate_to_dh');
+
+$args = array();
+while ($arg = drush_shift()) {
+  $args[] = $arg;
+}
+$pid = $args[0];
+
+echo "pid:" . $pid;
+
+?>
