9239bccfc059e2ae0f56f8c80cd979a793025d35
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2021-06-23 10:52:44 -0400

    setup

--- php/src/get_comids_shape.php
+++ php/src/get_comids_shape.php
@@ -2,7 +2,7 @@
 
 $noajax = 1;
 include_once('xajax_modeling.element.php');
-if (count($argv) < 1) {
+if (count($argv) < 2) {
   error_log("Return a st_UNION WKT given a list of comids");
   error_log("USAGE: php get_comids_shape.php name comids (csv) [debug=0]\n");
   die;
